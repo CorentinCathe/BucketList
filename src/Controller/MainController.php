3932949deb970023f95433419801949564fe33e5
cocat <co.cathelinais@gmail.com>
2021-09-21 11:31:40 +0200

    change home route and add active condition on nav item

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     /**
-     * @Route("/home", name="home")
+     * @Route("/", name="home")
      */
     public function home(): Response
     {
