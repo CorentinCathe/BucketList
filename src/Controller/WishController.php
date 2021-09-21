6e6b5f1445b3f3726370b1a976f0b5a5a6fa750e
cocat <co.cathelinais@gmail.com>
2021-09-21 16:04:47 +0200

    add everything to get info from db and display list and details

--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Wish;
+use App\Repository\WishRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,18 +13,24 @@ class WishController extends AbstractController
     /**
      * @Route("/wish-list", name="wish-list")
      */
-    public function wichList(): Response
+    public function wichList(WishRepository $repo): Response
     {
-        return $this->render('wish/list.html.twig');
+        $wishList = $repo->findAll();
+
+
+        return $this->render('wish/list.html.twig', [
+            'wishList' => $wishList
+        ]);
     }
 
     /**
-     * @Route("/details", name="details")
+     * @Route("/details/{id}", name="details")
      */
-    public function details(): Response
+    public function details(Wish $wish): Response
     {
+        // dd($wish);
         return $this->render('wish/details.html.twig', [
-            'controller_name' => 'WishController',
+            'wish' => $wish
         ]);
     }
 }
---
+++ src/Entity/Wish.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\WishRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=WishRepository::class)
+ */
+class Wish
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string", length=600, nullable=true)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $author;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     */
+    private $isPublished;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $dateCreated;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): self
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getIsPublished(): ?bool
+    {
+        return $this->isPublished;
+    }
+
+    public function setIsPublished(?bool $isPublished): self
+    {
+        $this->isPublished = $isPublished;
+
+        return $this;
+    }
+
+    public function getDateCreated(): ?\DateTimeInterface
+    {
+        return $this->dateCreated;
+    }
+
+    public function setDateCreated(?\DateTimeInterface $dateCreated): self
+    {
+        $this->dateCreated = $dateCreated;
+
+        return $this;
+    }
+}
